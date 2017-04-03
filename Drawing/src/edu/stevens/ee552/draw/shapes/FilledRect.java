f8641b6ee4ea9cf23eb37a07e729496d5514fc6b
tangchengjingwen <ctang8@stevens.edu>
2017-04-03 17:26:12 -0400

    Update FilledRect.java

--- Drawing/src/edu/stevens/ee552/draw/shapes/FilledRect.java
+++ Drawing/src/edu/stevens/ee552/draw/shapes/FilledRect.java
@@ -1,8 +1,33 @@
 package edu.stevens.ee552.draw.shapes;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class FilledRect extends Shape {
-	public void draw(Graphics g) {
-		
-		
+
+	protected FilledRect(int x1, int x2, int y1, int y2, Color edgeColor, Color filledColor) {
+		super(x1, x2, y1, y2, edgeColor, filledColor);
 	}
+
+	protected FilledRect(int x1, int x2, int y1, int y2) {
+		super(x1, x2, y1, y2);
+	}
+
+	public void draw(Graphics g) {
+        
+		g.fillRect(x1, y1, x2 - x1, y2 - y1);
+		
+		g.setColor(Color.RED);
+
+	}
+
+	@Override
+	public boolean isClicked(int x, int y) {
+		// TODO Auto-generated method stub
+		
+		
+		return false;
+	}
+	
+
 }
