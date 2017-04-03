cf385a2050afdda79ddf5aa573657ed5fc994c74
Xiaoshi Xu <xxu31@stevens.edu>
2017-04-03 16:59:27 -0400

    Shape Class

--- Drawing/src/edu/stevens/ee552/draw/shapes/Shape.java
+++ Drawing/src/edu/stevens/ee552/draw/shapes/Shape.java
@@ -1,9 +1,29 @@
 package edu.stevens.ee552.draw.shapes;
 
-public class Shape {
+import java.awt.Color;
+import java.awt.Graphics;
 
-	public void draw(Graphics g);
-	public static Shape createShape(int x1, int y1, int x2, int y2) {
+public abstract class Shape {
+
+
+	protected Color filledColor;
+	protected Color edgeColor;
+	protected int x1,y1,x2,y2;
+	protected Shape(int x1,int x2,int y1,int y2,Color edgeColor,Color filledColor){
+		this.x1=x1;
+		this.x2=x2;
+		this.y1=y1;
+		this.y2=y2;
+		this.edgeColor=edgeColor;
+		this.filledColor=filledColor;
 	}
+	protected Shape(int x1,int x2,int y1,int y2){
+		this.x1=x1;
+		this.x2=x2;
+		this.y1=y1;
+		this.y2=y2;
+	}
+	public abstract void draw(Graphics g);
+	
 	public abstract boolean isClicked(int x, int y);
 }
