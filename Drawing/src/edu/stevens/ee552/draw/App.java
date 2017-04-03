73ccf494057b09344129749b1eddc9d7e8c5b378
Sudhansh Aggarwal <sudhansh@sudhansh.com>
2017-04-03 16:42:47 -0400

    Initial structure, need to make the menu now

--- Drawing/src/edu/stevens/ee552/draw/App.java
+++ Drawing/src/edu/stevens/ee552/draw/App.java
@@ -3,7 +3,11 @@ package edu.stevens.ee552.draw;
 public class App extends JFrame {
 	private HashMap<String, ActionListener> actionMap;
 	public App(String title, int w, int h, String[][] menus, Color bgColor) {
-		
+		super(title);
+		setSize(w,h);
+		setBackground(bgColor);
+		//make the file menu etc
+		setVisible(True);
 	}
 	public App(String confFile) {
 		
