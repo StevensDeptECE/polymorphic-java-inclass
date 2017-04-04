bb65cf306f6f7d4dae3f8007d059020f07ed3e40
Chengjingwen Tang <ctang8@stevens.edu>
2017-04-04 13:02:18 -0400

    FilledRect

--- Drawing/src/edu/stevens/ee552/draw/shapes/FilledRect.java
+++ Drawing/src/edu/stevens/ee552/draw/shapes/FilledRect.java
@@ -4,30 +4,32 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class FilledRect extends Shape {
+	private int width, height;
 
-	protected FilledRect(int x1, int x2, int y1, int y2, Color edgeColor, Color filledColor) {
+	public FilledRect(int x1, int x2, int y1, int y2, Color edgeColor, Color filledColor) {
 		super(x1, x2, y1, y2, edgeColor, filledColor);
-	}
-
-	protected FilledRect(int x1, int x2, int y1, int y2) {
-		super(x1, x2, y1, y2);
+		this.width = x2 - x1;
+		this.height = y2 - y1;
 	}
 
 	public void draw(Graphics g) {
-        
-		g.fillRect(x1, y1, x2 - x1, y2 - y1);
-		
-		g.setColor(Color.RED);
+		g.setColor(edgeColor);
+		if (width > 0 && height > 0)
+			g.fillRect(x1, y1, width, height);
+		if (width < 0 && height > 0)
+			g.fillOval(x1 + width, y1, Math.abs(width), height);
+		if (width < 0 && height < 0)
+			g.fillOval(x1 + width, y1 + height, Math.abs(width), Math.abs(height));
+		if (width > 0 && height < 0)
+			g.fillOval(x1, y1 + height, width, Math.abs(height));
 
 	}
 
 	@Override
 	public boolean isClicked(int x, int y) {
 		// TODO Auto-generated method stub
-		
-		
+
 		return false;
 	}
-	
 
 }
