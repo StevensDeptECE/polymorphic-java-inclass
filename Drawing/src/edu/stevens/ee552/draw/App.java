bc2a6ddfd91f5bf92cef1a939e257294f83236ee
Sudhansh Aggarwal <sudhansh@sudhansh.com>
2017-04-10 20:08:42 -0400

    Changed making the window to a different function

--- Drawing/src/edu/stevens/ee552/draw/App.java
+++ Drawing/src/edu/stevens/ee552/draw/App.java
@@ -13,6 +13,50 @@ public class App extends JFrame {
 	private HashMap<String, ActionListener> actionMap;
 	public App(String title, int w, int h, String[][] menus, Color bgColor) {
 	
+		this.title=title;
+		//super(title);
+		//this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    this.w=w; this.h=h;
+		//this.setSize(w,h);
+	    //Container c = getContentPane();
+	    this.bgColor=bgColor;
+	    //c.setBackground(bgColor);
+
+	    //JPanel p = new JPanel();
+	    //p.setBackground(Color.black);
+	    //p.setLayout(new GridLayout(1,4));
+	    //for (int i-0;i<4;i++){
+	    //	JButton b = new JButton();
+	    //}
+	    
+		//make the file menu etc
+		//setVisible(True);
+	    make_it();
+	}
+	public App(String confFile) {
+		FileReader f = new FileReader(configfile);
+		String line;
+		while(line = f.readLine()){
+			String[] words = line.split("\\s+"); //takes out the problem of extra spaces
+			if (words[0]=="TITLE")
+				this.title=words[1];
+			else if (words[0]=="HEIGHT")
+				this.h=valueOf(words[1]);
+			else if (words[0]=="WIDTH")
+				this.w=valueOf(words[1]);
+			else if (words[0]=="bgColor")
+				this.bgColor=words[1];
+			//need to make the menu bar and shape selector bar
+			//else if (words[0]=="FILE")
+				//Add words[1]=hashmap
+			//else if (words[0]=="EDIT")
+				//Add words[1]=hashmap
+
+		}
+		make_it();
+	}
+	
+	private void make_it(){
 		super(title);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    this.setSize(w,h);
@@ -25,28 +69,7 @@ public class App extends JFrame {
 	    for (int i-0;i<4;i++){
 	    	JButton b = new JButton();
 	    }
-	    
 		//make the file menu etc
 		setVisible(True);
-	}
-	public App(String confFile) {
-		FileReader f = new FileReader(configfile);
-		String line;
-		while(line = f.readLine()){
-			String[] words = line.split("\\s+"); //takes out the problem of extra spaces
-			if (words[0]=="TITLE")
-				title=words[1];
-			else if (words[0]=="HEIGHT")
-				h=valueOf(words[1]);
-			else if (words[0]=="WIDTH")
-				w=valueOf(words[1]);
-			else if (words[0]=="bgColor")
-				bgColor=words[1];
-			//need to make the menu bar and shape selector bar
-			else if (words[0]=="FILE")
-				//Add words[1]=hashmap
-			else if (words[0]=="EDIT")
-				//Add words[1]=hashmap
-		}
 	}
 }
