7a6e6d150fdb157a1903bcc0779c0469ee312484
Sudhansh Aggarwal <sudhansh@sudhansh.com>
2017-04-10 20:58:26 -0400

    ERROR: App.java runs returns "Selection does not contain a main type"

--- Drawing/src/edu/stevens/ee552/draw/App.java
+++ Drawing/src/edu/stevens/ee552/draw/App.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 import java.io.*;
+import java.util.*;
 
 public class App extends JFrame {
 	String title; 
@@ -11,30 +12,32 @@ public class App extends JFrame {
 	String[][] menus; 
 	Color bgColor;
 	private HashMap<String, ActionListener> actionMap;
-	public App(String title, int w, int h, String[][] menus, Color bgColor) {
 	
-		this.title=title;
-		//super(title);
+	
+	//public App(String title, int w, int h, Color bgColor , String[][] menus ) {
+	public App(String title, int w, int h, Color bgColor ) {
+		//this.title=title;
+		super(title);
 		//this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    this.w=w; this.h=h;
 		//this.setSize(w,h);
 	    //Container c = getContentPane();
 	    this.bgColor=bgColor;
 	    //c.setBackground(bgColor);
-
 	    //JPanel p = new JPanel();
 	    //p.setBackground(Color.black);
 	    //p.setLayout(new GridLayout(1,4));
 	    //for (int i-0;i<4;i++){
 	    //	JButton b = new JButton();
 	    //}
-	    
 		//make the file menu etc
 		//setVisible(True);
 	    make_it();
 	}
+	//Constructor to read form a file and make the environment
+	/*
 	public App(String confFile) {
-		FileReader f = new FileReader(configfile);
+		FileReader f = new FileReader(confFile);
 		String line;
 		while(line = f.readLine()){
 			String[] words = line.split("\\s+"); //takes out the problem of extra spaces
@@ -45,7 +48,8 @@ public class App extends JFrame {
 			else if (words[0]=="WIDTH")
 				this.w=valueOf(words[1]);
 			else if (words[0]=="bgColor")
-				this.bgColor=words[1];
+				this.bgColor = (Color)Color.class.getField(words[1]).get(null);
+				//this.bgColor=words[1];
 			//need to make the menu bar and shape selector bar
 			//else if (words[0]=="FILE")
 				//Add words[1]=hashmap
@@ -54,22 +58,32 @@ public class App extends JFrame {
 
 		}
 		make_it();
-	}
+	}   */
 	
 	private void make_it(){
-		super(title);
+		//Makes the window as per the specifications given
+		//super(title);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    this.setSize(w,h);
 	    Container c = getContentPane();
 	    c.setBackground(bgColor);
 
 	    JPanel p = new JPanel();
-	    p.setBackground(Color.black);
+	    p.setBackground(Color.BLUE);
 	    p.setLayout(new GridLayout(1,4));
-	    for (int i-0;i<4;i++){
-	    	JButton b = new JButton();
-	    }
+//	    for (int i=0;i<4;i++){
+//	    	JButton b = new JButton();
+//	    }
+	    JButton b = new JButton("test button");
+	    JLabel l = new JLabel("test label");
+	    p.add(b);
+	    p.add(l);
+	    this.add(p);
 		//make the file menu etc
-		setVisible(True);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args){
+		new App("Test title", 500, 700, Color.RED);
 	}
 }
