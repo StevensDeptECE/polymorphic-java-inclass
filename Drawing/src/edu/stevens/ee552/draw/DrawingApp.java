1e08638e3c40ba9213bd21f77afa6b4551f1e361
yeo09898 <sye5@stevens.edu>
2017-04-12 14:55:08 -0400

    extends App add actionlistener to open&save, leave note

--- Drawing/src/edu/stevens/ee552/draw/DrawingApp.java
+++ Drawing/src/edu/stevens/ee552/draw/DrawingApp.java
@@ -1,102 +1,139 @@
 package edu.stevens.ee552.draw;
+
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.*;
 import javax.swing.*;
 import java.io.*;
 import javax.imageio.*;
 
+public class DrawingApp extends App { // we need a method to repaint image from DrawArea
+	private JMenuBar bar = new JMenuBar();
+	private JMenu File = new JMenu("File");
+	private File file;
+	private JMenuItem Open = new JMenuItem("Open");
+	private JMenuItem Save = new JMenuItem("Save");
+	private JMenuItem Quit = new JMenuItem("Quit");
+	private JButton Line = new JButton("Line");
+	private JButton Rect = new JButton("Rect");
+	private JButton fRect = new JButton("filled Rect");
+	private JButton Ellipse = new JButton("Ellipse");
+	private JButton fEllipse = new JButton("filled Ellipse");
+	private BufferedImage bufImg=null;
 
-public class DrawingApp extends JFrame implements ActionListener{ //extends App {
-	JMenuBar bar = new JMenuBar();
-	JMenu File = new JMenu("File");
-	JMenuItem Open = new JMenuItem("Open");
-	JMenuItem Save = new JMenuItem("Save");
-	JMenuItem Quit = new JMenuItem("Quit");
-	JButton Line = new JButton("Line");
-	JButton Rect = new JButton("Rect");
-	JButton fRect = new JButton("filled Rect");
-	JButton Ellipse = new JButton("Ellipse");
-	JButton fEllipse = new JButton("filled Ellipse");
 	public DrawingApp() {
-		super("drawing");
-		setSize(1000,700);
+		super("drawing",1366,768,Color.WHITE);
 		setJMenuBar(bar);
+		Container c = getContentPane();
 		File.add(Open);
 		File.add(Save);
 		File.add(Quit);
 		bar.add(File);
+		c.add(BorderLayout.CENTER, new DrawArea());
 		actions();
-		Container c = getContentPane();
-		c.add(BorderLayout.CENTER, new DrawArea());	
-		JPanel panel = new JPanel(new GridLayout(5,1));
+		JPanel panel = new JPanel(new GridLayout(5, 1));
 		panel.add(Line);
 		panel.add(Rect);
 		panel.add(fRect);
 		panel.add(Ellipse);
 		panel.add(fEllipse);
-		c.add("West",panel);
+		c.add(BorderLayout.WEST, panel);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
-	public void actions(){
-		Open.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				
-			}
-		});
-		Save.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				
-			}
-		});
-		Quit.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				
-			}
-		});
-		Line.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				
-			}
-		});
-		Rect.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				
-			}
-		});
-		fRect.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				
-			}
-		});
-		Ellipse.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				
-			}
-		});
-		fEllipse.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				
-			}
-		});
-		
 
-		
-		
-		
-		
+	public void actions() {
+		JFileChooser jfc=new JFileChooser(); 
+		jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES );  
+		Open.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			        jfc.showDialog(new JLabel(), "Ñ¡Ôñ"); 
+					if (file==null)
+						return;
+					else
+						file=jfc.getSelectedFile();
+					try {
+						bufImg = ImageIO.read(file);
+					}catch (FileNotFoundException e1) {
+						e1.printStackTrace();
+					}catch (IOException e1) {
+						e1.printStackTrace();
+					}
+					System.out.println(file);
+				}
+
+			
+		});
+		Save.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			   int select = jfc.showSaveDialog(DrawingApp.this);
+//             fileChooser.setSelectedFile(new File("new.txt")); 
+               File file = null;
+               
+               String fileName = null;
+               if(select==JFileChooser.APPROVE_OPTION){
+                   file =jfc.getSelectedFile();   
+               }
+               fileName = jfc.getName(file);
+               if(fileName==null|| fileName.trim().length()==0){
+                   JOptionPane.showMessageDialog(DrawingApp.this, "Empty!");
+               }
+               
+               if(file.isFile()){
+                   fileName = file.getName();
+               }
+               file = jfc.getCurrentDirectory();
+               
+               String path = file.getPath()+fileName;
+               file =new File(path);
+           
+                if(file.exists()) { 
+                    int i = JOptionPane.showConfirmDialog(DrawingApp.this, "File exist, do you want to replace it ?");     
+                    if(i == JOptionPane.YES_OPTION)   ;     
+                    else   return ;    
+                    } 
+				try {
+					ImageIO.write(bufImg, "jpeg", new File("test1.jpg"));
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		Quit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+		Line.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+		Rect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+		fRect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+		Ellipse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+		fEllipse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+
 	}
 
 	public static void main(String[] args) {
 		new DrawingApp();
-	}
-
-
-
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
